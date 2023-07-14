a0cee603a188f44dfcd77e3a7e012c176383accc
dariom25 <dario@maciey.de>
2023-07-14 15:40:03 +0200

    Fix that the correct number of div is created

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let gridSize = 16
 
 function createGrid(sizeOfGrid) {
-    for (let i = 1; i < sizeOfGrid; i++) {
+    for (let i = 0; i < sizeOfGrid; i++) {
         const squareDiv = document.createElement("div")
         squareDiv.classList.add("square-div")
+        squareDiv.textContent = "Test"
         const gridContainer = document.querySelector(".container")
         gridContainer.appendChild(squareDiv)
     }
-} 
+}
+
+createGrid(gridSize);
