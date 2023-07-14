474f51e98f534bfb1b8f2ce252d8270ecaeb5922
dariom25 <dario@maciey.de>
2023-07-14 18:09:26 +0200

    Delete test text

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let gridSide = 4
+let gridSide = 32
 
 function createGrid(sizeOfGrid) {
     for (let i = 0; i < sizeOfGrid; i++) {
@@ -9,7 +9,6 @@ function createGrid(sizeOfGrid) {
         for (let i = 0; i < sizeOfGrid; i++) {
             const squareDiv = document.createElement("div");
             squareDiv.classList.add("square-div");
-            squareDiv.textContent = "Test";
             const rowContainers = document.querySelectorAll(".row-container");
             rowContainers.forEach((rowContainer) => {
                 rowContainer.appendChild(squareDiv);
